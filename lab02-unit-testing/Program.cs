7689221a94c612c978e497dc66c19e9792956e34
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-20 11:55:18 -0700

    Build out functioning initial UI interaction sequence

--- lab02-unit-testing/Program.cs
+++ lab02-unit-testing/Program.cs
@@ -2,7 +2,7 @@
 
 namespace lab02_unit_testing
 {
-    class Program
+    public class Program
     {
         public static decimal balance = 500m;
 
@@ -10,22 +10,68 @@ namespace lab02_unit_testing
         /// Handles control flow of user interaction and calls helper methods
         /// </summary>
         /// <param name="args">string[]</param>
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the ATM game!");
-            Console.WriteLine($"Your balance is {balance:C2}.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Enter the number of the action you would like to select.");
 
             try
             {
                 // Set boolean to true for keeping program running
+                bool runATM = true;
 
                 // Use while loop to continue allowing user actions while boolean set to true
+                while (runATM == true)
+                {
+                    Console.WriteLine("   1. View balance\n" +
+                        "   2. Withdraw funds\n" +
+                        "   3. Deposit funds\n" +
+                        "   4. Quit ATM session");
 
-                // Set boolean to false and end program when user says so
+                    string userAction = Console.ReadLine();
 
-                // 
+                    switch (userAction)
+                    {
+                        case "1":
+                            Console.WriteLine("You've selected \"View balance\".");
+                            DisplayBalance();
+                            break;
 
+                        case "2":
+                            Console.WriteLine("You've selected \"Withdraw funds\".");
+                            // Call WithdrawFunds
 
+                            break;
+
+                        case "3":
+                            Console.WriteLine("You've selected \"Deposit funds\".");
+                            // Call DepositFunds
+
+                            break;
+
+                        case "4":
+                            runATM = false;
+                            Console.WriteLine("You've selected \"Quit ATM session\".");
+                            break;
+
+                        default:
+                            Console.WriteLine("Please enter just a number 1-4.");
+                            break;
+                    }
+
+                    // Give user option to end ATM session or initiate new session
+                    Console.WriteLine(" ");
+                    Console.WriteLine("To start a new ATM session, type the number of the action you'd like to do.\nOr hit Enter to exit the program. ");
+                    userAction = Console.ReadLine();
+                    if (userAction != "1" &&
+                        userAction != "2" &&
+                        userAction != "3" &&
+                        userAction != "4")
+                    {
+                        runATM = false;
+                    }
+                }
             }
             catch (Exception e)
             {
@@ -41,11 +87,17 @@ namespace lab02_unit_testing
         /////////////////////////////
         // Helper methods
         /////////////////////////////
-        
-        // Calc balance
 
-        // View balance
+        // Calc/update balance
+
+        // DisplayBalance
+        static void DisplayBalance()
+        {
+        
             // Display using currency format string
+            Console.WriteLine($"Your balance is {balance:C2}.");
+
+        }
 
         // WithdrawFunds
             // Update balance
