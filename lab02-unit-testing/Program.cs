7fc8329dbc0f70d50fe328712304b5301a9ea73d
Andrew Curtis <curtis.andrewm@gmail.com>
2019-03-20 10:38:09 -0700

    Add initial ATM lab project and unit testing project

--- lab02-unit-testing/Program.cs
+++ lab02-unit-testing/Program.cs
@@ -4,9 +4,16 @@ namespace lab02_unit_testing
 {
     class Program
     {
+        public static decimal balance = 500m;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the ATM game!");
+            Console.WriteLine($"Your balance is {balance:C2}.");
+
+
+
+            Console.ReadLine();
         }
     }
 }
